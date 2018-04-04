59e7770bcfedc7979131bae7d221594fc41f17b8
devin <deivnobowen@gmail.com>
2018-04-04 16:49:46 -0600

    done with simulations today

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -16,7 +16,19 @@ export default class Menu extends Component {
     render() {
         return (
             <div className="menu">
-                menu
+                <div className="menu_left">
+                    <img src={require("../assets/logo.svg")} alt=""/>
+                    <div>Menu<img src={require("../assets/menu.svg")} alt=""/></div>
+                    <div>cart<img src={require("../assets/shopping-cart.svg")} alt=""/></div>
+                    <div>Logout<img src={require("../assets/logout.svg")} alt=""/></div>
+                </div>
+                <div className="menu_right">
+                    <div id='menu_main'>
+                        <div>1</div>
+                        <div>2</div>
+                        <div>3</div>
+                    </div>
+                </div>
             </div>
         );
     }
